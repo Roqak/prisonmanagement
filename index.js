32fb21f4618c0661dd7a05b879c11711eb7df7dd
Roqak <roqak1@gmail.com>
2019-01-29 12:37:44 +0100

    Added Handlebars

--- index.js
+++ index.js
@@ -2,8 +2,13 @@ const express = require('express');
 const app = express();
 const port = 3000
 const mongoose = require('mongoose')
+var expressHbs = require('express-handlebars');
+app.engine('.hbs', expressHbs({defaultLayout: 'layout', extname: '.hbs'}));
+app.set('view engine', '.hbs');
 
-
+app.get('/',(req,res)=>{
+    res.render('index',)
+})
 
 
 app.listen(port,()=>{
